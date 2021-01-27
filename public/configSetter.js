af6f7e97ee2bc902fba1e0c705f967ac31c22afe
ren <rectheworld@gmail.com>
2021-01-27 11:49:58 -0800

    added endpoints for other modules

--- public/configSetter.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-var head= document.getElementsByTagName('head')[0];
-var script= document.createElement('script');
-script.type= 'text/javascript';
-script.src= process.env.REN_MODULE;
-head.appendChild(script);
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const server = express();
 
@@ -13,11 +14,59 @@ server.get('/', cors(), (req, res) => {
   res.json(`yo`)
 });
 
+server.get('/bundleash', (req, res) => {
+  const config = {
+    method: 'get',
+    url: process.env.ASH_MODULE,
+    headers: { },
+  };
+
+  axios(config)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(`An error occured ${error}`);
+    });
+});
 
 server.get('/bundleren', (req, res) => {
   const config = {
     method: 'get',
-    url: 'http://ec2-52-87-237-8.compute-1.amazonaws.com:3001/bundle',
+    url: process.env.REN_MODULE,
+    headers: { },
+  };
+
+  axios(config)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(`An error occured ${error}`);
+    });
+});
+
+server.get('/bundlejavier', (req, res) => {
+  const config = {
+    method: 'get',
+    url: process.env.JAVIER_MODULE,
+    headers: { },
+  };
+
+  axios(config)
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      res.send(`An error occured ${error}`);
+    });
+});
+
+
+server.get('/bundlejosh', (req, res) => {
+  const config = {
+    method: 'get',
+    url: process.env.JOSH_MODULE,
     headers: { },
   };
 
